38defccea64db4db2f379f893fa8541abfee2db6
otegami <a.s.takuya1026@gmail.com>
2022-01-21 20:54:39 +0900

    Return and handle an error

--- create_a_go_module/greetings/greetings.go
+++ create_a_go_module/greetings/greetings.go
@@ -1,12 +1,15 @@
 package greetings
 
 import (
+	"errors"
+
 	"fmt"
 )
 
-func Hello(name string) string {
-	// var message string
-	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
